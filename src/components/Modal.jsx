8a432b1317f27fad5ac6e9ea04691e3f510b9a21
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-09 19:46:32 +0530

    Using useEffect for Syncing with Browser APIs and Understanding Effect Dependencies

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,19 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 function Modal({ openIt, children}) {
   const dialog = useRef();
 
+  useEffect(() => {
+    if(openIt) {
+    dialog.current.showModal();
+  } else {
+    dialog.current.close();
+  }
+  }, [openIt]);
   
-
   return createPortal(
-    <dialog className="modal" ref={dialog} open={openIt}>
+    <dialog className="modal" ref={dialog} >
       {children}
     </dialog>,
     document.getElementById('modal')
